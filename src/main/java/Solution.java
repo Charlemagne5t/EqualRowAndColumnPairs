1b9111b66070a04326b68d18effd5074270c9958
alek <wait82985@gmail.com>
2023-06-13 10:38:01 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,31 @@
+import java.util.*;
+
 public class Solution {
     public int equalPairs(int[][] grid) {
         //TODO
-        return 0;
+        int m = grid.length;
+        Map<List<Integer>, Integer> rows = new HashMap<>();
+        List<List<Integer>> columns = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < m; j++) {
+                row.add(grid[i][j]);
+                if (columns.size() == j) {
+                    columns.add(new ArrayList<>());
+                }
+                columns.get(j).add(grid[i][j]);
+            }
+            if (rows.containsKey(row)) {
+                int val = rows.get(row);
+                val++;
+                rows.put(row, val);
+            } else rows.put(row, 1);
+        }
+        int count = 0;
+        for (List<Integer> column : columns) {
+            count += rows.getOrDefault(column, 0);
+        }
+        return count;
     }
 }
